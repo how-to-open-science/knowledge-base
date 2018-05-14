6a25af29460cd6654494c64d3c8751750cc70ce6
Felix Henninger <mailbox@felixhenninger.com>
2018-05-15 00:29:47 +0200

    Customize header and footer

---
+++ docs/_scripts/credits.js
@@ -0,0 +1,30 @@
+{
+  const el = document.querySelector('[data-md-component="credits"]')
+  const repo = 'felixhenninger/open-science-knowledge-base'
+  const creditsPrepend = '<small>Curated by</small><div>'
+  const creditsAppend = '</div>'
+
+  if (el) {
+    // Download contributor data
+    fetch(`https://api.github.com/repos/${ repo }/contributors`)
+      .then(r => r.json())
+      .then(contributors => {
+        // Build footer contents
+        el.innerHTML = creditsPrepend + contributors
+          .filter(c => c.login !== 'gitbook-bot')
+          .map(c => `<a href="${ c.html_url }">
+            <img
+              src="${ c.avatar_url }"
+              class="credits-image"
+              alt="${ c.login }"
+            >
+          </a>`)
+          .join('') + creditsAppend
+      })
+      .then(() => {
+        // Show footer
+        document.querySelector('.md-footer-credits')
+          .style.display = 'block'
+      })
+  }
+}
